c61f83e5efc161a30095d50fc47d0067dd9b8bba
Larissa Souza <larissa2004.souza@gmail.com>
2021-09-13 11:18:53 -0300

    Upload Versão Desktop: Base

---
+++ Projeto/PB2/js/main.js
@@ -0,0 +1,96 @@
+/*  Abre e fecha o menu ao clicar no hamburguinho e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
+
+/* Esconder o menu ao clicar em um item dele */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
+    })
+}
+
+/* Quando a página der scroll, modifica o header */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+    if (window.scrollY >= navHeight) {
+        // scroll é maior que a altura do header
+        header.classList.add('scroll')
+    } else {
+        // menor que a altura do header
+        header.classList.remove('scroll')
+    }
+}
+
+
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(
+    `#home .text, #home .image,  #home .retangule,
+  #about heading, #about .image, #about .text,
+  #skills .text, #skills .image, #skills .lang .box .icon,
+  #projects .text, #projects .project,
+  #contact .text, #contact .links, #contact .form,
+  footer .brand, footer .social
+  `, { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+    if (window.scrollY >= 560) {
+        backToTopButton.classList.add('show')
+    } else {
+        backToTopButton.classList.remove('show')
+    }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if (checkpointStart && checkpointEnd) {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.add('active')
+        } else {
+            document
+                .querySelector('nav ul li a[href*=' + sectionId + ']')
+                .classList.remove('active')
+        }
+    }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
+    activateMenuAtCurrentSection()
+})
